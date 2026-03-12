0e077def35d65ecf91a8022cc0972d18baff40a4
Vivek08v <vivekkumar08v@gmail.com>
2026-03-12 18:28:05 +0530

    first commit

---
+++ src/main/java/com/example/vahaklocationservice/VahakLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.vahaklocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VahakLocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VahakLocationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vahaklocationservice/VahakLocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vahaklocationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VahakLocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
